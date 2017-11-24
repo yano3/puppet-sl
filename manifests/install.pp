03713829521c4a8fef2719164424d2e0db516def
Teppei Yano <buckelwal@gmail.com>
2017-11-24 13:30:44 +0900

    pdk create class install

---
+++ manifests/install.pp
@@ -0,0 +1,10 @@
+# sl::install
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include sl::install
+class sl::install {
+}
